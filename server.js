c627afb70d2711da7a5e4cfc87e4bb6dc7f712f7
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2023-09-23 19:54:18 +0900

    fix: EJS framework orqali ananaviy frontend quramiz

--- server.js
+++ server.js
@@ -15,12 +15,19 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
-app.get("./hello", function(req, res) {
-    res.end(`<h1>HELLO WORLD by John</h1>`);
-});
+// app.get("/hello", function (req, res) {
+//     res.end(`<h1>HELLO WORLD by John</h1>`);
+// });
 
-app.get("./gift", function(req, res) {
-    res.end(`<h1>Siz sovgalar bolimidasz</h1>`);
+// app.get("/gift", function (req, res) {
+//     res.end(`<h1>Sz sovgalar bolimidasz</h1>`);
+// });
+app.post("/create-item", (req, res) => {
+console.log(req);
+res.json({test: "success"});
+})
+app.get("/", function (req, res) {
+    res.render("harid");
 });
 
 const server = http.createServer(app);
