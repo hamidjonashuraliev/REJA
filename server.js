a9bb28e08b73d137cca84dca02dfbc81c030c0c3
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2023-09-26 09:30:16 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 console.log("Web serverni boshlash");
 const express = require("express");
-const  app = express();
+const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 // 1: KIrish code
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // 2: section  code
 
-// 3: views code 
+// 3: views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
@@ -23,17 +32,18 @@ app.set("view engine", "ejs");
 //     res.end(`<h1>Sz sovgalar bolimidasz</h1>`);
 // });
 app.post("/create-item", (req, res) => {
-console.log(req);
-res.json({test: "success"});
-})
+    // TOTO: code with db here
+});
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+});
+
 app.get("/", function (req, res) {
     res.render("harid");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function() {
+server.listen(PORT, function () {
     console.log(`The server is running successfully on port: ${PORT}`);
 });
-
-
