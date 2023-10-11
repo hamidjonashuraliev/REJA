5be4455d6b26080f681e0de91a1f05931b311843
hamidjonashuraliev <khamidjonashuraliev033@gmail.com>
2023-10-11 22:13:57 +0900

    fix: modify port 7010

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const connectionString =
     "mongodb+srv://John:MS9rm7BBeNbjnpLD@cluster0.gmbdcan.mongodb.net/REJA?retryWrites=true&w=majority&appName=AtlasApp";
 mongodb.connect(
     connectionString,
-    { useNewUrlParser: true, 
-        useUnifiedTopology: true,
-     },
+    { useNewUrlParser: true, useUnifiedTopology: true },
     (err, client) => {
         if (err) console.log("ERROR on connection MongoDB");
         else {
@@ -17,7 +15,7 @@ mongodb.connect(
             module.exports = client;
             const app = require("./app");
             const server = http.createServer(app);
-            let PORT = 3000;
+            let PORT = 7010;
             server.listen(PORT, function () {
                 console.log(
                     `The server is running successfully on port:${PORT}, http://localhost:${PORT}`
